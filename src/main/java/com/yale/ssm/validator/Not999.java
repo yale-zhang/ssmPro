3ee7590ccbe1fcf9b7b64bcbb731bdda706a8585
zys6024 <zys6024@163.com>
2018-03-06 18:24:38 +0800

    自定义validator标签

---
+++ src/main/java/com/yale/ssm/validator/Not999.java
@@ -0,0 +1,19 @@
+package com.yale.ssm.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+/**
+ * 自定义validator标签(和hibernate validator组合使用)
+ *
+ */
+@Constraint(validatedBy = Not999Validator.class)//具体实现
+public @interface Not999 {
+    //提示信息 可以写死 可填写国际化key
+    String message() default "{com.yale.ssm.validator.Not999}";
+
+    //下面这2个属性必须添加
+    Class<?>[] group() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/com/yale/ssm/validator/Not999Validator.java
@@ -0,0 +1,20 @@
+package com.yale.ssm.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class Not999Validator implements ConstraintValidator<Not999,Long> {
+    @Override
+    public void initialize(Not999 not999) {
+
+    }
+
+    @Override
+    public boolean isValid(Long aLong, ConstraintValidatorContext context) {
+        if(aLong==999) {
+            return false;
+        }else {
+            return true;
+        }
+    }
+}
