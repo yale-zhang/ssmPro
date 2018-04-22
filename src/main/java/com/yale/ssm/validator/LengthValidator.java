b47f80451b7ef083ca125ab07733c62f37db35c9
zhangsy <zys6024@163.com>
2018-04-23 02:47:09 +0800

    业务校验

--- src/main/java/com/yale/ssm/validator/LengthValidator.java
+++ src/main/java/com/yale/ssm/validator/LengthValidator.java
@@ -1,7 +1,10 @@
 package com.yale.ssm.validator;
 
 import com.yale.ssm.entity.User;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class LengthValidator<T extends User> implements IValidator<T>{
@@ -10,4 +13,25 @@ public class LengthValidator<T extends User> implements IValidator<T>{
     public void validate(T user) {
 
     }
+
+
+   /* public static String lengthLimit(String source, String arg, int max) {
+        if (StringUtils.isNotBlank(source) && source.trim().length() > max) {
+            throw new BusinessException(String.format("[%s]长度超出了限制,最大为:%s", arg, max));
+        } else {
+            return source;
+        }
+    }
+
+    public static BigDecimal lengthLimit(BigDecimal source, String arg, BigDecimal max) {
+        if (source != null && max != null) {
+            if (source.compareTo(max) == 1) {
+                throw new BusinessException(String.format("[%s]长度超出了限制,最大为:%s", arg, max));
+            } else {
+                return source;
+            }
+        } else {
+            return source;
+        }
+    }*/
 }
